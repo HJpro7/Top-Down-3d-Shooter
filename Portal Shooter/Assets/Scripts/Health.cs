90881dfb44ebb729c902db98d0c1262cfad3fccd
HesaraJ <hesaraja@icloud.com>
2018-04-18 23:35:51 +0800

    Finished up tutorial

---
+++ Portal Shooter/Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Health : NetworkBehaviour
+{
+
+    public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+            return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+
+            RpcRespawn();
+        }
+    }
+
+    void OnChangeHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if(isLocalPlayer){
+            transform.position = Vector3.zero;
+        }
+    }
+}
